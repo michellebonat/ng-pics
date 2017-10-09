788a700cafd9096833175cf4c0449a5a38ee3522
michellebonat <michelle.bonat@gmail.com>
2017-10-09 10:19:59 -0700

    First commit, app runs locally using Bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppBootstrapModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
